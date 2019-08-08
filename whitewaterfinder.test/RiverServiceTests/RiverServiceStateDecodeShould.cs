004be5929a337c161f30d7229cae96515747f9a8
Luke Badgerow <badgerow.luke@gmail.com>
2019-08-07 21:31:55 -0400

    starting to get the tests that I've neglected also going to handle retrys etc.

--- whitewaterfinder.test/RiverServiceTests/RiverServiceStateDecodeShould.cs
+++ whitewaterfinder.test/RiverServiceTests/RiverServiceStateDecodeShould.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using whitewaterfinder.BusinessObjects.Rivers;
 using Xunit;
+using Moq;
+using System.Threading.Tasks;
+
 namespace whitewaterfinder.test.RiverServiceTests
 {
     public class RiverServiceStateDecodeShould : BaseRiverServiceTests
@@ -21,6 +26,37 @@ namespace whitewaterfinder.test.RiverServiceTests
             var val = service.GetStateCode("West Virginia");
             Assert.Equal("WV", val);
         }
+        [Fact]
+        public  void ReturnAnEnumerableOfRiverWithPartialName()
+        {
+            MockRiverRepo.Setup(m =>  m.GetRiversAsync(It.IsAny<string>())).ReturnsAsync(new List<River>());
+            var vals = service.GetRivers("gaul");
+            Assert.IsType<List<River>>(vals);
+        }
+        [Fact] 
+        public void ReturnsAnEnumerableOfRiverWithStateCode()
+        {
+            MockRiverRepo.Setup(m =>  m.GetRiversByState(It.IsAny<string>())).Returns(new List<River>());
+            var vals = service.GetRivers("MD");
+            Assert.IsType<List<River>>(vals);
+
+        }
+        [Fact]
+        public void ReturnsAnEnumerableOfRiversWithEmptyString()
+        {
+            MockRiverRepo.Setup(m =>  m.GetRivers()).Returns(new List<River>());
+            var vals = service.GetRivers(string.Empty);
+            Assert.IsType<List<River>>(vals);
+
+        }
+        [Fact]
+        public void ReturnRiverDetails()
+        {
+            MockDetailRepo.Setup(m => m.GetRiverDetailsAsync(It.IsAny<string>())).ReturnsAsync(new River());
+            var vals = service.GetRiverDetails("4444444");
+            Assert.IsType<River>(vals);
+        }
+
 
     }
 
