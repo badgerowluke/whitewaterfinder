c8b5b0f5b69006ae4dc651c10d7c73fb31a38053
Luke <badgerow.luke@gmail.com>
2019-08-22 13:01:20 -0400

    added the monitor to solution and set up IoC

--- whitewaterfinder.api.monitoring/QueueMonitor.cs
+++ whitewaterfinder.api.monitoring/QueueMonitor.cs
@@ -2,11 +2,16 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.ApplicationInsights;
 
 namespace whitewaterfinder.api.monitoring
 {
-    public static class QueueMonitor
+    public class QueueMonitor
     {
+        public QueueMonitor()
+        {
+            
+        }
         private static readonly TelemetryClient tc = new TelemetryClient();
         [FunctionName("QueueMonitor")]
         public static void Run([TimerTrigger("0 */30 * * * *")]TimerInfo myTimer, ILogger log)
---
+++ whitewaterfinder.api.monitoring/Startup.cs
@@ -0,0 +1,18 @@
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using System;
+
+[assembly: FunctionsStartup(typeof(whitewaterfinder.api.monitoring.Startup))]
+
+namespace whitewaterfinder.api.monitoring
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder();
+        }
+    }
+}
