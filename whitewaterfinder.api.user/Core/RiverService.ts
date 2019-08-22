dfd3d4b6c5349671d1a3c59106250278a3133488
Luke <badgerow.luke@gmail.com>
2019-08-22 10:16:40 -0400

    this gets the queue and the post work moved out of the function

--- whitewaterfinder.api.user/Core/RiverService.ts
+++ whitewaterfinder.api.user/Core/RiverService.ts
@@ -1,4 +1,55 @@
-import * as azure from "azure-storage";
+import {TableService, QueueService, ExponentialRetryPolicyFilter, createTableService,createQueueService, TableUtilities } from "azure-storage";
+import * as uuidv1 from 'uuid/v1';
 export class RiverService {
+    private service: TableService;
+    private queue: QueueService;
+
+    constructor(private connectionString:string) {
+        const retryOptions = new ExponentialRetryPolicyFilter();
+        this.service = createTableService(connectionString).withFilter(retryOptions);
+        this.queue = createQueueService(connectionString).withFilter(retryOptions);
+        
+    }
+    postToStorage: (myQueueItem:string) => void = (myQueueItem:string) => {
+        this.service.createTableIfNotExists("UserPreferences", (error) =>{
+            if(error) {
+                console.log(error);
+                throw new Error("oops");
+            }
+            return;
+        });
+
+        let entGen = TableUtilities.entityGenerator;
+        let pref = {
+            PartitionKey: entGen.String(myQueueItem["name"]),
+            RowKey: entGen.String(uuidv1()),
+            FavoriteRiver: entGen.String(myQueueItem["favoriteRiver"])
+        };
+        this.service.insertOrMergeEntity("UserPreferences",pref, (error, result, response) =>{
+            if(error) {
+                console.log(error);
+                throw new Error("uh-oh");
+            }
+        })        
+
+    }
+    getFromStorage  = async(): Promise<any> =>{
+        return null;
+    }
+    postToQueue = (record:any) =>  {
+        this.queue.createQueueIfNotExists("user-preference-queue", (error, results, response)=>{
+            if(!error) {
+                console.log("queue created");
+            }
+            return;
     
+        });
+        this.queue.createMessage("user-preference-queue", Buffer.from(JSON.stringify(record)).toString('base64'), (error, results, response) =>{
+            if(error) {
+                console.log("something went wrong");
+            }
+        });
+    }
+
+
 }
--- whitewaterfinder.api.user/WriteUserPreferences/index.ts
+++ whitewaterfinder.api.user/WriteUserPreferences/index.ts
@@ -1,36 +1,12 @@
 import { AzureFunction, Context } from "@azure/functions"
-import * as azure from "azure-storage";
-import * as uuidv1 from 'uuid/v1';
-
 import * as stuff from "../Core/RiverService"
 
 
 const queueTrigger: AzureFunction = async function (context: Context, myQueueItem: string): Promise<void> {
     context.log('Queue trigger function processed work item', myQueueItem);
 
-    let retryOptions = new azure.ExponentialRetryPolicyFilter();
-    const serve = new stuff.RiverService();
-    let tableService = azure.createTableService(process.env["blob-store"]).withFilter(retryOptions);
-    tableService.createTableIfNotExists("UserPreferences", (error, result, response) => {
-        if(error) {
-            console.log(error);
-            throw new Error("oops");
-        }
-        return;
-    });
-
-    let entGen = azure.TableUtilities.entityGenerator;
-    let pref = {
-        PartitionKey: entGen.String(myQueueItem["name"]),
-        RowKey: entGen.String(uuidv1()),
-        FavoriteRiver: entGen.String(myQueueItem["favoriteRiver"])
-    };
-    tableService.insertOrMergeEntity("UserPreferences",pref, (error, result, response) =>{
-        if(error) {
-            console.log(error);
-            throw new Error("uh-oh");
-        }
-    })
+    const serve = new stuff.RiverService(process.env["blob-store"]);
+    serve.postToStorage(myQueueItem)
 };
 
 export default queueTrigger;
