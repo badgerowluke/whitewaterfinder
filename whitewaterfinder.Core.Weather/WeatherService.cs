01e595cb0fd0529209487845cf9cb5d6fb4c49f1
Luke Badgerow <Luke.Badgerow@insight.com>
2019-10-02 08:07:55 -0400

    get the weather logic started

---
+++ whitewaterfinder.Core.Weather/WeatherService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace whitewaterfinder.Core.Weather
+{
+    public class WeatherService
+    {
+        /*
+            https://www.weather.gov/documentation/services-web-api#
+
+            1. get location
+                https://api.weather.gov/points/39.8522449,-82.8868636
+            
+            2. get forecast for that location
+                https://api.weather.gov/gridpoints/ILN/88,76/forecast
+            
+
+
+            "https://api.weather.gov/gridpoints/ILN/37,37/forecast",
+            "https://api.weather.gov/stations/KLUK/observations/current"
+        */
+    }
+}
