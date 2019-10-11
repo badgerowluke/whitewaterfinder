143fb86a45e4abaf5cf11e6678554bd663f1e09a
Luke Badgerow <Luke.Badgerow@insight.com>
2019-10-11 11:45:55 -0400

    this didn't solve anything

--- whitewaterfinder.app.bot/Program.cs
+++ whitewaterfinder.app.bot/Program.cs
@@ -23,10 +23,6 @@ namespace whitewaterfinder.app.bot
                     logging.AddConsole();
                 })
                 .UseApplicationInsights()
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    config.AddEnvironmentVariables();
-                })
                 .UseStartup<Startup>();
     }
 }
