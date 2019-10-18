678df31dd87d2b2f411ce1c0ed633b8c8f848674
Luke Badgerow <Luke.Badgerow@insight.com>
2019-10-18 09:00:49 -0400

    lets see if this gets a decent secrete passed.

--- whitewaterfinder.infrastructure/deployment.ps1
+++ whitewaterfinder.infrastructure/deployment.ps1
@@ -8,13 +8,14 @@ param (
 Write-Output 'logging out'
 az logout
 Write-Output 'logging in'
-
+$cred = Get-Credential
 Write-Output 'got credentials'
 Write-Output 'Connecting to Azure RM'
 try 
 {
-    az login --service-principal --username $user --password $pass --tenant $tenantid 
-
+    # az login --service-principal --username $user --password $pass --tenant $tenantid --allow-no-subscriptions
+    # az account set --subscription aea3a7b3-756a-48fd-8d43-52a0c6e3c877
+    Connect-AzureRmAccount -Credential $cred -TenantId $tenantid -ServicePrincipal
     
 } catch 
 {
