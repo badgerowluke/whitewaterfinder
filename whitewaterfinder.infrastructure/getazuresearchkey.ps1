3c6a74dc55f99b5bef05ce621a5b9bd399e48ef5
Luke Badgerow <badgerow.luke@gmail.com>
2020-03-06 16:47:52 -0500

    well.  not sure what I actually need.

--- whitewaterfinder.infrastructure/getazuresearchkey.ps1
+++ whitewaterfinder.infrastructure/getazuresearchkey.ps1
@@ -5,7 +5,10 @@ $searchService = $resourceGroup + "search"
 Write-Output $searchService
 $key = az search query-key list --resource-group $resourceGroup --service-name $searchService | ConvertFrom-Json
 Write-Output $key.key
+
+
+$keyValue = $key.key
 Write-Host "##vso[task.setvariable variable=searchKey;]$key.key"
 Get-ChildItem Env:
 
-Write-Host "Key is: ($key.key)"
+Write-Host "Key is: ($keyValue)"
