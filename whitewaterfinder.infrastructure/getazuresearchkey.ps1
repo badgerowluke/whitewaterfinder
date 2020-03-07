7da157eb2da02f8e63526381ddafd1f1ce756782
Luke Badgerow <badgerow.luke@gmail.com>
2020-03-07 12:30:37 -0500

    just for giggles.

--- whitewaterfinder.infrastructure/getazuresearchkey.ps1
+++ whitewaterfinder.infrastructure/getazuresearchkey.ps1
@@ -1,10 +1,23 @@
+function Get-AzureSearchServiceName([string] $rgName)
+{
+    $service = az search service list --resource-group $rgName | ConvertFrom-Json
+    return $service.name
+}
+
+
+
 param (
     [Parameter(Mandatory=$true)] $resourceGroup
 )
+$seervName = Get-AzureSearchServiceName($resourceGroup)
+Write-Host $seervName
+
+
 $searchService = $resourceGroup + "search"
 Write-Output $searchService
 $key = az search query-key list --resource-group $resourceGroup --service-name $searchService | ConvertFrom-Json
 Write-Output $key.key
+
 
 
 $keyValue = $key.key
