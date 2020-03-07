ff70442408875a464a62ffa7d74e944fbc0cd2fc
Luke Badgerow <badgerow.luke@gmail.com>
2020-03-07 09:32:59 -0500

    maybe it's just an issue with calling back into the cli?

--- whitewaterfinder.infrastructure/getazuresearchkey.ps1
+++ whitewaterfinder.infrastructure/getazuresearchkey.ps1
@@ -9,6 +9,17 @@ Write-Output $key.key
 
 $keyValue = $key.key
 Write-Host "##vso[task.setvariable variable=AZURE_SEARCH_KEY;]$keyValue"
+
+
+
+$service = az search service list --resource-group $resourceGroup | ConvertFrom-Json
+Write-Output $service.name
+
+$serviceName = $service.name
+$keys = az search admin-key show --output json --resource-group $resourceGroup --service-name 
+
+
+
 Get-ChildItem Env:
 
 Write-Host "Key is: ($keyValue)"
