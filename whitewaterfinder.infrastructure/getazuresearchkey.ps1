8ebfc34d4553e9aed7ea6f15ebb2b6a203180082
Luke Badgerow <badgerow.luke@gmail.com>
2020-03-07 09:26:48 -0500

    lets try this.  just to see

--- whitewaterfinder.infrastructure/getazuresearchkey.ps1
+++ whitewaterfinder.infrastructure/getazuresearchkey.ps1
@@ -6,6 +6,10 @@ Write-Output $searchService
 $key = az search query-key list --resource-group $resourceGroup --service-name $searchService | ConvertFrom-Json
 Write-Output $key.key
 
+$service = az search service list --resource-group $resourceGroup | ConvertFrom-Json
+Write-Output $service.name
+$serviceName = $service.name
+$keys = az search admin-key show --output json --resource-group $resourceGroup --service-name 
 
 $keyValue = $key.key
 Write-Host "##vso[task.setvariable variable=AZURE_SEARCH_KEY;]$keyValue"
