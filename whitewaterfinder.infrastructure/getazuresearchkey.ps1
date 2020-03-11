0cf5025395fbb38c0c21e794afc93aa3e36d570e
Luke Badgerow <badgerow.luke@gmail.com>
2020-03-10 21:53:08 -0400

    need to continue on error after this.

--- whitewaterfinder.infrastructure/getazuresearchkey.ps1
+++ whitewaterfinder.infrastructure/getazuresearchkey.ps1
@@ -2,11 +2,10 @@ param (
     [Parameter(Mandatory=$true)] $resourceGroup
 )
 
-$searchService = az search service list --resource-group $resourceGroup | ConvertFrom-Json
 
 
-Write-Output $searchService
-$key = az search query-key list --resource-group $resourceGroup --service-name $searchService.name | ConvertFrom-Json
+
+$key = az search query-key list --resource-group $resourceGroup --service-name 'waterfindersearch' | ConvertFrom-Json
 Write-Output $key.key
 
 
