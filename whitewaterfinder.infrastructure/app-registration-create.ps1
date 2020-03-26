f997d818cf5342ed39c3d577f0e79ae128e91c8c
Luke Badgerow <badgerow.luke@gmail.com>
2020-03-26 19:06:59 -0400

    this should clear up the "already created" issue.

--- whitewaterfinder.infrastructure/app-registration-create.ps1
+++ whitewaterfinder.infrastructure/app-registration-create.ps1
@@ -2,8 +2,30 @@ param (
   [Parameter(Mandatory=$true)]  $botName
 )
 
-$val = az ad app create --display-name $botName --password "tacospastapizza4@11" `
-         --available-to-other-tenants | ConvertFrom-Json
+$apps = az ad app list | ConvertFrom-Json
+$doCreate = $true
+$val = $null
+
+
+foreach($app in $apps)
+{
+  if($app.displayName -eq $botName)
+  {
+
+    $doCreate = $false
+    $val =  $app
+    break
+  }
+
+}
+
+if($doCreate)
+{
+  $val = az ad app create --display-name $botName --password "tacospastapizza4@11" `
+           --available-to-other-tenants | ConvertFrom-Json
+
+}
+
 
 
 $newAppId =  $val.appId
