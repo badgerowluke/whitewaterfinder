4cd55f3abee1191bdbfa5f703ea7f5d2fd60cd31
Luke Badgerow <badgerow.luke@gmail.com>
2020-07-28 08:35:55 -0400

    need to check if this clears the weirdness with App Insights setup

---
+++ whitewaterfinder.infrastructure/storage/create-storage-containers.ps1
@@ -0,0 +1,27 @@
+
+
+param (
+    [Parameter(Mandatory=$true)] $templateOutput,
+    [Parameter(Mandatory=$true)] $containerName,
+    [Parameter(Mandatory=$true)] $tableOrQueue
+)
+$json = $templateOutput | ConvertFrom-Json
+
+
+$key =  $json.storageKey.Value
+$container = $json.storageAccountName.Value
+
+
+
+if( "table" -eq $tableOrQueue) {
+    az storage table create -n $containerName `
+    --account-key $key `
+    --account-name $container
+
+}
+
+if( "queue" -eq $tableOrQueue) {
+    az storage queue create -n $containerName `
+    --account-key $key `
+    --account-name $container
+}re4
