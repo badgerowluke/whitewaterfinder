f5b02fc625ea9199e157dff9fb625298141e3fc1
Luke Badgerow <badgerow.luke@gmail.com>
2020-08-05 12:15:32 -0400

    work to try and get the environment.ts values OUT of source control

---
+++ whitewaterfinder.pwa/paddle-finder/build.ts
@@ -0,0 +1,27 @@
+import { writeFile } from 'fs'
+const colors = require('colors');
+require('dotenv').config();
+// require('dotenv').load();
+console.log(colors.green("hello world"))
+
+const targetPath = './src/environments/environment.ts';
+
+// `environment.ts` file structure
+const envConfigFile = `export const environment = {
+    production: ${process.env.production},
+    baseUrl: '${process.env.baseUrl}',
+    riverKeyCode: '',
+    detailsKeyCode:'',
+    auth0Domain: '${process.env.auth0Domain}',
+    auth0ClientId: '${process.env.auth0ClientId}',
+    openIdScope: '${process.env.openIdScope}'
+ };
+ `;
+
+ writeFile(targetPath, envConfigFile, function (err) {
+    if (err) {
+        throw console.error(err);
+    } else {
+        console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+ });
